9c86378323378efd0d7d735ba27ccec30f17783a
Guillermo Perez <gperez78@gmail.com>
2023-04-20 18:14:15 -0300

    Server bootstrap and handshake

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// TODO: remove the ! assertion
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
