00b95695be7b2e29a4d96a2d7b637d257fae59ad
Guillermo Perez <gperez78@gmail.com>
2023-04-24 18:24:42 -0300

    Test rejectino reasons endpoint

---
+++ server/src/candidates/rejectionReason.test.ts
@@ -0,0 +1,18 @@
+import rejectionReasonService from "./rejectionReason.service";
+
+describe("get all rejectino reasons", () => {
+  it("should return a list of rejection reasons", () => {
+    const result = rejectionReasonService.all();
+
+    expect(result.length).toBe(8);
+  });
+
+  it("should return rejection reasons with the expected fields", () => {
+    const result = rejectionReasonService.all();
+
+    expect(result[0]).toEqual({
+      id: 1,
+      description: "Cantidad de materias aprobadas fuera de lo deseado",
+    });
+  });
+});
